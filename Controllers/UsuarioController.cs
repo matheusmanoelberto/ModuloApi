1ed5f4253123d4d956c855f2c8ca4d58f3e59d60
matheusmanoelberto <matheusmanoel51@gmail.com>
2023-10-23 18:56:17 -0300

    class contato

---
+++ Controllers/UsuarioController.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ModuloApi.Controllers
+{
+    // [Route]
+    // public class UsuarioController
+    // {
+    //     [HttpGet("obterDataHotaAtual")]
+    //     public IActionResult ObterDataHora()
+    //     {
+    //         var obj = new
+    //         {
+    //             Data = DateTime.Now.ToShortDateString(),
+    //             Hora = DateTime.Now.ToShortTimeString()
+    //         };
+
+    //         return Ok(obj);
+    //     }
+    // }
+}
